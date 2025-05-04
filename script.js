0e27a68fca8f61266ed396a118688f78311e0cbd
Faizan Ahmad <fiaznhmd.0624@gmail.com>
2025-05-04 12:02:22 +0530

    breakpts finish

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const sendEmail = (e) => {
 };
 
 contactForm.addEventListener('submit', sendEmail);
-console.log(contactForm);
+// console.log(contactForm);
 
 // scrollup
 const scrollUp = () => {
@@ -35,3 +35,25 @@ const scrollUp = () => {
     : scrollUp.classList.remove('show-scroll');
 };
 window.addEventListener('scroll', scrollUp);
+
+//scroll section active link
+
+const sections = document.querySelectorAll('section[id');
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id'),
+      sectionsClass = document.querySelector(
+        '.nav_list a[href*=' + sectionId + ']'
+      );
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link');
+    } else {
+      sectionsClass.classList.remove('active-link');
+    }
+  });
+};
+window.addEventListener('scroll', scrollActive);
